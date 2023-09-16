92bc1aa64de864af89ef25872a910724526601ab
nsyafiq32 <53846768+nsyafiq32@users.noreply.github.com>
2023-09-17 02:01:04 +0700

    added style, script. for index added image, button, and more codes

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const navPersegi = document.querySelector("nav button:nth-child(1)");
+const navPersegiPanjang = document.querySelector("nav button:nth-child(2)");
+const navSegitiga = document.querySelector("nav button:nth-child(3)");
+const sectionPersegi = document.querySelector("section:persegi");
+const sectionPersegiPanjang = document.querySelector("section:persegi-panjang");
+const sectionSegitiga = document.querySelector("section:segitiga");
+
+navPersegi.addEventListener("click", () => {
+    sectionPersegi.classList.add("active");
+    sectionPersegiPanjang.classList.remove("active");
+    sectionSegitiga.classList.remove("active");
+});
+
+navPersegiPanjang.addEventListener("click", () => {
+    sectionPersegiPanjang.classList.add("active");
+    sectionPersegi.classList.remove("active");
+    sectionSegitiga.classList.remove("active");
+});
+
+navSegitiga.addEventListener("click", () => {
+    sectionSegitiga.classList.add("active");
+    sectionPersegi.classList.remove("active");
+    sectionPersegiPanjang.classList.remove("active");
+});
+
+const btnSubmit = document.getElementsByClassName("btn-submit");
+const side1 = document.getElementsByClassName("side1");
+const side2 = document.getElementsByClassName("side2");
+const side3 = document.getElementsByClassName("side3");
+
+btnSubmit[0].addEventListener("click", (e) => {
+    if(sectionPersegi.contains("active")) {
+        e.preventDefault();
+        countLuasPersegi();
+        countKelilingPersegi();
+    }
+});
+
+btnSubmit[1].addEventListener("click", (e) => {
+    if(sectionPersegiPanjang.contains("active")) {
+        e.preventDefault();
+        countLuasPersegiPanjang();
+        countKelilingPersegiPanjang();
+    }
+});
+
+btnSubmit[2].addEventListener("click", (e) => {
+    if(sectionSegitiga.contains("active")) {
+        e.preventDefault();
+        countLuasSegitigaa();
+        countKelilingSegitiga();
+    }
+});
+
+function countLuasPersegi() {
+    const inputValue = document.getElementById("sisi-persegi").value;
+    side1[0].innerText = inputValue;
+
+    document.querySelector("section:persegi").innerText = inputValue * inputValue;
+}
